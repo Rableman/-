ac94817956ae6e12956fa452a2107b4362a81443
klarss <shadow96neko@gmail.com>
2019-11-19 09:58:39 +0900

    Update route.py

--- search_route/route.py
+++ search_route/route.py
@@ -3,108 +3,125 @@ import queue
 import sys
 import numpy as np
 
-H = 11 #N*Mのグリッド
-W = 11
-field = list() #グリッド上の情報を格納
-Route_Field = np.zeros((H,W),dtype=int) #最終経路の表示に使用
-CL = np.zeros((H,W), dtype=int) #CLは2次元配列で用意
-OL = queue.Queue() #キューはモジュールを用いる
-Cost = np.zeros((H,W), dtype=int) + 999 #経路を辿るためのコスト
-Start = (1,1) #Start位置とGoal位置はあらかじめタプル型で用意
-Goal = (9,9)
+class route_data:
+    def __init__(self):
+        self.H = 11 #N*Mのグリッド
+        self.W = 11
+        self.field = list() #グリッド上の情報を格納
+        self.Route_Field = np.zeros((self.H,self.W),dtype=int) #最終経路の表示に使用
+        self.CL = np.zeros((self.H,self.W), dtype=int) #CLは2次元配列で用意
+        self.OL = queue.Queue() #キューはモジュールを用いる
+        self.Cost = np.zeros((self.H,self.W), dtype=int) + 999 #経路を辿るためのコスト
+        self.Start = (1,1) #Start位置とGoal位置はあらかじめタプル型で用意
+        self.Goal = (9,9)
+        self.route = [self.Goal]
 
-def next(x):
-    #上に進めるか判定
-    if(field[x[1]-1][x[0]]==0 and CL[x[1]-1][x[0]]==0):
-        OL.put((x[0],x[1]-1))
-        Cost[x[1]-1][x[0]] = Cost[x[1]][x[0]] + 1 
-    #下に進めるか判定
-    if(field[x[1]+1][x[0]]==0 and CL[x[1]+1][x[0]]==0):
-        OL.put((x[0],x[1]+1))
-        Cost[x[1]+1][x[0]] = Cost[x[1]][x[0]] + 1
-    #右に進めるか判定
-    if(field[x[1]][x[0]+1]==0 and CL[x[1]][x[0]+1]==0):
-        OL.put((x[0]+1,x[1]))
-        Cost[x[1]][x[0]+1] = Cost[x[1]][x[0]] + 1
-    #左に進めるか判定
-    if(field[x[1]][x[0]-1]==0 and CL[x[1]][x[0]-1]==0):
-        OL.put((x[0]-1,x[1]))
-        Cost[x[1]][x[0]-1] = Cost[x[1]][x[0]] + 1
-    return
+    def next(self,x):
+        #上に進めるか判定
+        if(self.field[x[1]-1][x[0]]==0 and self.CL[x[1]-1][x[0]]==0):
+            self.OL.put((x[0],x[1]-1))
+            self.Cost[x[1]-1][x[0]] = self.Cost[x[1]][x[0]] + 1 
+        #下に進めるか判定
+        if(self.field[x[1]+1][x[0]]==0 and self.CL[x[1]+1][x[0]]==0):
+            self.OL.put((x[0],x[1]+1))
+            self.Cost[x[1]+1][x[0]] = self.Cost[x[1]][x[0]] + 1
+        #右に進めるか判定
+        if(self.field[x[1]][x[0]+1]==0 and self.CL[x[1]][x[0]+1]==0):
+            self.OL.put((x[0]+1,x[1]))
+            self.Cost[x[1]][x[0]+1] = self.Cost[x[1]][x[0]] + 1
+        #左に進めるか判定
+        if(self.field[x[1]][x[0]-1]==0 and self.CL[x[1]][x[0]-1]==0):
+            self.OL.put((x[0]-1,x[1]))
+            self.Cost[x[1]][x[0]-1] = self.Cost[x[1]][x[0]] + 1
+        return
 
-#迷路の情報をfieldにリストで格納
-f = open('field.txt','r')
-for row in f:
-    row_re = row.replace('\n', '')
-    row_new = list()
-    for i in row_re: #文字から数値に変換
-        row_new.append(int(i))
-    field.append(list(row_new))
-f.close()
+    def load_map(self):
+        #迷路の情報をfieldにリストで格納
+        f = open('field.txt','r')
+        for row in f:
+            row_re = row.replace('\n', '')
+            row_new = list()
+            for i in row_re: #文字から数値に変換
+                row_new.append(int(i))
+            self.field.append(list(row_new))
+        f.close()
 
-OL.put(Start) #OLに初期状態を追加
-Cost[Start[1]][Start[0]] = 0
+    def format_OL(self):
+        self.OL.put(self.Start) #OLに初期状態を追加
+        self.Cost[self.Start[1]][self.Start[0]] = 0
 
-#while文で探索する
-while not OL.empty():
-    x=OL.get()
-    CL[x[1]][x[0]]=1
-    #Goalについた時の判定
-    if(x == Goal):
-        break
-    next(x)
+    def search_route(self):
+        #while文で探索する
+        while not self.OL.empty():
+            x = self.OL.get()
+            self.CL[x[1]][x[0]]=1
+            #Goalについた時の判定
+            if(x == self.Goal):
+                break
+            Route.next(x)
+        if(x!=self.Goal):
+            print("Fault\n")
+            sys.exit()
 
-if(x!=Goal):
-    print("Fault\n")
-    sys.exit()
+    def reverse_route(self):
+        # ゴールから逆順でルート計算
+        point_now = self.Goal
+        cost_now = self.Cost[self.Goal[0], self.Goal[1]]
+        while cost_now > 0:
+            #上から来た場合
+            try:
+                if self.Cost[point_now[0] - 1][ point_now[1]] == cost_now - 1:
+                    #更新
+                    point_now = (point_now[0] - 1, point_now[1])
+                    cost_now = cost_now - 1
+                    #記録
+                    self.route.append(point_now)
+            except: pass
+            #下から来た場合
+            try:
+                if self.Cost[point_now[0] + 1, point_now[1]] == cost_now - 1:
+                    #更新
+                    point_now = (point_now[0] + 1, point_now[1])
+                    cost_now = cost_now - 1
+                    #記録
+                    self.route.append(point_now)
+            except: pass
+            #左から来た場合    
+            try:
+                if self.Cost[point_now[0], point_now[1] - 1] == cost_now - 1:
+                    #更新
+                    point_now = (point_now[0], point_now[1] - 1)
+                    cost_now = cost_now - 1
+                    #記録
+                    self.route.append(point_now)
+            except: pass
+            #右から来た場合
+            try:
+                if self.Cost[point_now[0], point_now[1] + 1] == cost_now - 1:
+                    #更新
+                    point_now = (point_now[0], point_now[1] + 1)
+                    cost_now = cost_now - 1
+                    #記録
+                    self.route.append(point_now)
+            except: pass
+        #ルートを逆順にする
+        self.route = self.route[::-1]
 
-# ゴールから逆順でルート計算
-point_now = Goal
-cost_now = Cost[Goal[0], Goal[1]]
-route = [Goal]
-while cost_now > 0:
-    #上から来た場合
-    try:
-        if Cost[point_now[0] - 1][ point_now[1]] == cost_now - 1:
-            #更新
-            point_now = (point_now[0] - 1, point_now[1])
-            cost_now = cost_now - 1
-            #記録
-            route.append(point_now)
-    except: pass
-    #下から来た場合
-    try:
-        if Cost[point_now[0] + 1, point_now[1]] == cost_now - 1:
-            #更新
-            point_now = (point_now[0] + 1, point_now[1])
-            cost_now = cost_now - 1
-            #記録
-            route.append(point_now)
-    except: pass
-    #左から来た場合    
-    try:
-        if Cost[point_now[0], point_now[1] - 1] == cost_now - 1:
-            #更新
-            point_now = (point_now[0], point_now[1] - 1)
-            cost_now = cost_now - 1
-            #記録
-            route.append(point_now)
-    except: pass
-    #右から来た場合
-    try:
-        if Cost[point_now[0], point_now[1] + 1] == cost_now - 1:
-            #更新
-            point_now = (point_now[0], point_now[1] + 1)
-            cost_now = cost_now - 1
-            #記録
-            route.append(point_now)
-    except: pass
+    def update_route(self):
+        #通る経路を1で埋める
+        for route_i in self.route:
+            self.Route_Field[route_i[0]][route_i[1]] = 1
 
-#ルートを逆順にする
-route = route[::-1]
+    def show_route(self):
+        #探索したルートを表示
+        for i in self.Route_Field:
+            print(*i)
 
-for route_i in route:
-    Route_Field[route_i[0]][route_i[1]] = 1
+Route = route_data()
 
-for i in Route_Field:
-    print(*i)
+Route.load_map()
+Route.format_OL()
+Route.search_route()
+Route.reverse_route()
+Route.update_route()
+Route.show_route()
