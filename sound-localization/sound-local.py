639948d59ac61c0468e235460e034be1d537ebde
Rableman <haruki_yokota@hotmail.co.jp>
2019-11-22 11:19:58 +0900

    added sound-local

--- sound-localization/sound-local.py
+++ sound-localization/sound-local.py
@@ -1,21 +1,14 @@
-#Taken from http://takeshid.hatenadiary.jp/entry/2015/11/27/045123
-#プロット関係のライブラリ
-import pyqtgraph as pg
-from pyqtgraph.Qt import QtCore, QtGui
 import numpy as np
-import sys
-
-#音声関係のライブラリ
 import pyaudio
 import struct
+import matplotlib.pyplot as plt
 
-
-class PlotWindow:
+class PlotFreq:
     def __init__(self):
         #マイクインプット設定
-        self.CHUNK=512           #1度に読み取る音声のデータ幅
+        self.CHUNK=256           #1度に読み取る音声のデータ幅
         self.RATE=44100             #サンプリング周波数
-        self.update_seconds=10      #更新時間[ms]
+        self.record_seconds=1      #録音時間[ms]
         self.audio=pyaudio.PyAudio()
         self.stream=self.audio.open(format=pyaudio.paInt16,
                                     channels=1,
@@ -26,28 +19,33 @@ class PlotWindow:
         #音声データの格納場所(プロットデータ)
         self.data=np.zeros(self.CHUNK)
         self.axis=np.fft.fftfreq(len(self.data), d=1.0/self.RATE)
+        self.frq_data = []
+        self.db_data = []
 
-        #プロット初期設定
-        self.win=pg.GraphicsWindow()
-        self.win.setWindowTitle("SpectrumAnalyzer")
-        self.plt=self.win.addPlot() #プロットのビジュアル関係
-        self.plt.setYRange(0,100)    #y軸の制限
-
-        #アップデート時間設定
-        self.timer=QtCore.QTimer()
-        self.timer.timeout.connect(self.update)
-        self.timer.start(self.update_seconds)    #10msごとにupdateを呼び出し
-
-    def update(self):
-        self.data=np.append(self.data,self.AudioInput())
-        if len(self.data)/self.CHUNK > 10:
-            self.data=self.data[self.CHUNK:]
+    def record(self):
+        print("recstart")
+        self.data=np.zeros(self.CHUNK)
+        for i in range(0, int(self.RATE/self.CHUNK * self.record_seconds)):
+            self.data=np.append(self.data,self.AudioInput())
         self.fft_data=self.FFT_AMP(self.data)
         self.axis=np.fft.fftfreq(len(self.data), d=1.0/self.RATE)
         for i in range (len(self.fft_data)):
-            if self.axis[i] < 700:
+            if self.axis[i] < 10000:
                 self.fft_data[i] = 0
-        self.plt.plot(x=self.axis, y=self.fft_data, clear=True, pen="y")  #symbol="o", symbolPen="y", symbolBrush="b")
+                self.axis[i] = 0
+        self.axis = self.axis[self.axis.nonzero()]
+        self.fft_data = self.fft_data[self.fft_data.nonzero()]
+        plt.plot(self.axis, self.fft_data, label="test")
+        plt.show()
+        self.frq_data.append(self.axis[list(self.fft_data).index((max(self.fft_data)))])
+        self.db_data.append(max(self.fft_data))
+
+
+    def end_rec(self):
+        self.stream.stop_stream()
+        self.stream.close()
+        self.audio.terminate()
+
 
     def AudioInput(self):
         ret=self.stream.read(self.CHUNK, exception_on_overflow = False)    #音声の読み取り(バイナリ) CHUNKが大きいとここで時間かかる
@@ -63,6 +61,11 @@ class PlotWindow:
         return data
 
 if __name__=="__main__":
-    plotwin=PlotWindow()
-    if (sys.flags.interactive!=1) or not hasattr(QtCore, 'PYQT_VERSION'):
-        QtGui.QApplication.instance().exec_()
+    plotwin=PlotFreq()
+    for i in range(0,1):
+        plotwin.record()
+    print(plotwin.db_data)
+    #plt.plot(plotwin.db_data, label = "db data")
+    #plt.show()
+
+    plotwin.end_rec()
