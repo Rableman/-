721d5a6e5976654920ec0a2b03ef6866b3710a2c
klarss <shadow96neko@gmail.com>
2019-11-30 18:55:45 +0900

    Update random_setup.py

--- search_route/random_setup.py
+++ search_route/random_setup.py
@@ -1,7 +1,8 @@
 #coding utf-8
 import random
- 
-def rand_map(min,max):
-    x = random.randint(min,max)
-    y = random.randint(min,max)
-    return x,y 
+
+class random_set:
+    def rand_map(min,max):
+        x = random.randint(min,max)
+        y = random.randint(min,max)
+        return x,y 
