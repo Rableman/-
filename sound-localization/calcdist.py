357a7a43f4657da7ab20cde73ce0128c70f5e0f2
Rableman <haruki_yokota@hotmail.co.jp>
2019-12-06 23:36:24 +0900

    ちょっと整理

--- sound-localization/calcdist.py
+++ sound-localization/calcdist.py
@@ -5,39 +5,25 @@ import rec_sound as rec
 class Calcdist: 
 
     def __init__(self):
+
         #音源周波数
+        #第１音源
         self.freqA = 18400
-        self.sourceA = [0, 50]
+        self.sourceA = [0, 5]
+        #self.funcA = rec.get_funcを使う
+        #第２音源
         self.freqB = 18700
-        self.sourceB = [50, 50]
+        self.sourceB = [5, 5]
+        #self.funcB = 
+        #第３音源
         self.freqC = 15000
-        self.sourceC = [50, 0]
+        self.sourceC = [5, 0]
+        #self.funcC = 
 
         #位置座標情報
         self.grid = self.init_grid()
-
-    def init_grid(self):
-        grid_num = 5
-        grid = [[0 for i in range(grid_num)] for i in range(grid_num)]
-        return grid
-
-    def set_grid(self, coord_x, coord_y):
-        self.init_grid()
-        self.grid[coord_x][coord_y] = 1
-
-    def show_grid(self):
-        self.set_grid(0,0)
-        for x in range(5):
-            print(self.grid[x])
-
     
-    def get_coord(self):
-        dist_a = self.calc_dist(rec.Record.record(self.freqA), self.freqA)
-        dist_b = self.calc_dist(rec.Record.record(self.freqB), self.freqB)
-        
-        x, y = self.calc_coord(dist_a, self.cos_rule(50, dist_a, dist_b))
-        return x, y
-
+    #音の振幅を元に距離を計算 
     def calc_dist(self, amp, freq):
         #11000Hz: y = -28.85ln(x) + 83.158
         #21000Hz: y = -32.8ln(x) + 60.863
@@ -50,11 +36,26 @@ class Calcdist:
         print("%2.2lf[cm]" % dist)
         return dist
 
+    #余弦定理
     def cos_rule(self, a, b, c):
         ret = (a**2 + c**2 - b**2)/(2 * a * c)
         ret = math.acos(ret)
         return ret
+    
+    '''座標関連は保留中
+    #座標初期化
+    def init_grid(self):
+        grid_num = 5
+        grid = [[0 for i in range(grid_num)] for i in range(grid_num)]
+        return grid
 
+    #座標表示
+    def show_grid(self):
+        self.set_grid(0,0)
+        for x in range(5):
+            print(self.grid[x])
+
+    #座標算出
     def calc_coord(self, dist, ang):
         u = dist * math.cos(ang)
         v = dist * math.sin(ang)
@@ -65,3 +66,12 @@ class Calcdist:
         x = coord[0]
         y = coord[1]
         return x, y
+
+    #座標提供
+    def get_coord(self):
+        dist_a = self.calc_dist(rec.Record.record(self.freqA), self.freqA)
+        dist_b = self.calc_dist(rec.Record.record(self.freqB), self.freqB)
+        
+        x, y = self.calc_coord(dist_a, self.cos_rule(50, dist_a, dist_b))
+        return x, y
+    '''
