b7711142e061cd54d235a6ee46f6301d93050c69
klarss <shadow96neko@gmail.com>
2019-12-10 16:47:53 +0900

    Update main.py

--- search_route/main.py
+++ search_route/main.py
@@ -3,9 +3,9 @@
 import route_proc
 import map_proc
 
-Device1 = route_proc.route_data(7,7,(1,1))
-Device1.Goal = (4,4)
-Device1.route = [Device1.Goal]
+Device1 = route_proc.route_data(7,7,(1,2))
+Device1.Goal = (4,3)
+Device1.route.append(Device1.Goal)
 Device_info = [Device1] #Device_infoというリストにすべてのデバイスのオブジェクトを入れる
 map_proc.map_func.load_map('field.txt',Device_info[0].field)
 Device_info[0].format_OL()
