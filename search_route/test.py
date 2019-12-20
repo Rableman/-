ebb82ec69b534aa1a38b0c2c49c248571baac46a
klarss <shadow96neko@gmail.com>
2019-12-20 16:50:57 +0900

    Update test.py

--- search_route/test.py
+++ search_route/test.py
@@ -3,16 +3,16 @@
 import route_proc
 import map_proc
 import goal_proc
+import common_proc
 
-
-
-goal_field = list()
-map_proc.map_func.load_map('goal_1.txt',goal_field)
+Common_data = common_proc.common_data(1)
+map_proc.map_func.load_map('goal_1.txt',Common_data.Goal_field)
 Device1 = route_proc.route_data(7,7,(4,1))
-Device_info = [Device1] #Device_infoというリストにすべてのデバイスのオブジェクトを入れる
-goal_proc.goal_func.search_goal(goal_field,Device_info,1)
+#Device_infoというリストにすべてのデバイスのオブジェクトを入れる
+Device_info = [Device1]
+goal_proc.goal_func.search_goal(Common_data.ret_goal_field(),Device_info,Common_data.ret_dev_sum())
 
-for i in range (1):
+for i in range (Common_data.ret_dev_sum()):
     map_proc.map_func.load_map('field.txt',Device_info[i].field)
     Device_info[i].search_route('field.txt',Device_info[i],Device_info[i].field)
     Device_info[i].update_route()
