84a6ab39c2db1198558d250cf0a64c765c6931ee
klarss <shadow96neko@gmail.com>
2019-12-20 15:26:18 +0900

    Update test_main.py

--- search_route/test_main.py
+++ search_route/test_main.py
@@ -12,10 +12,7 @@ Device_info = [Device1,Device2] #Device_infoというリストにすべてのデ
 goal_proc.goal_func.search_goal(goal_field,Device_info,2)
 
 for i in range (2):
-    Device_info[i].route.append(Device_info[i].Goal)
     map_proc.map_func.load_map('field.txt',Device_info[i].field)
-    #Device_info[i].format_OL()
     Device_info[i].search_route('field.txt',Device_info[i],Device_info[i].field)
-    #Device_info[i].reverse_route()
     Device_info[i].update_route()
     Device_info[i].show_route()
