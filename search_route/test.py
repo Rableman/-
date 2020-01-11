0307d44bc3336f1e5dfda413317bb9b1f6725848
klarss <shadow96neko@gmail.com>
2020-01-11 23:45:26 +0900

    Update test.py

--- search_route/test.py
+++ search_route/test.py
@@ -5,7 +5,7 @@ import map_proc
 import goal_proc
 import common_proc
 
-Common_data = common_proc.common_data(1)
+Common_data = common_proc.common_data(1,7,7)
 map_proc.map_func.load_map('goal_1.txt',Common_data.Goal_field)
 Device1 = route_proc.route_data(7,7,(4,1))
 #Device_infoというリストにすべてのデバイスのオブジェクトを入れる
