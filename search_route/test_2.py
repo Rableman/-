790d1afe69b189dd42b6f058ef57c251c724d1fb
klarss <shadow96neko@gmail.com>
2020-01-11 21:52:09 +0900

    Update test_2.py

--- search_route/test_2.py
+++ search_route/test_2.py
@@ -16,12 +16,17 @@ goal_proc.goal_func.search_goal(Common_data.ret_goal_field(),Device_info,Common_
 Common_data.init_step()
 
 for i in range (Common_data.ret_dev_sum()):
+    #ゴールのフィールドを設定
     Device_info[i].field = Common_data.Goal_field
+    #デバイスのゴールを決定する
     map_proc.map_func.del_my_goal(Device_info[i])
+    #デバイスのルートを策定する
     Device_info[i].search_route(Device_info[i],Device_info[i].field)
+    #通る道を1で埋める
     Device_info[i].update_route()
-    Device_info[i].show_route()
     #初期位置をゴールに初期化
     Common_data.set_start(Device_info[i])
     #ステップに書き込み
     map_proc.map_func.write_step(Device_info[i])
+    #ルートを表示
+    Device_info[i].show_route()
