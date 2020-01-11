16a78d3d8c2f392a4ee50c01d08eb2db30346399
klarss <shadow96neko@gmail.com>
2020-01-11 13:13:10 +0900

    Create test_2.py

---
+++ search_route/test_2.py
@@ -0,0 +1,27 @@
+#coding utf-8
+
+import route_proc
+import map_proc
+import goal_proc
+import common_proc
+
+Common_data = common_proc.common_data(2)
+map_proc.map_func.load_map('goal_2.txt',Common_data.Goal_field)
+Device1 = route_proc.route_data(7,7,(1,2))
+Device2 = route_proc.route_data(7,7,(5,4))
+Device_info = [Device1,Device2] #Device_infoというリストにすべてのデバイスのオブジェクトを入れる
+goal_proc.goal_func.search_goal(Common_data.ret_goal_field(),Device_info,Common_data.ret_dev_sum())
+
+#ループ分の外でstep_n.txtを初期化する
+Common_data.init_step()
+
+for i in range (Common_data.ret_dev_sum()):
+    Device_info[i].field = Common_data.Goal_field
+    map_proc.map_func.del_my_goal(Device_info[i])
+    Device_info[i].search_route(Device_info[i],Device_info[i].field)
+    Device_info[i].update_route()
+    Device_info[i].show_route()
+    #初期位置をゴールに初期化
+    Common_data.set_start(Device_info[i])
+    #ステップに書き込み
+    map_proc.map_func.write_step(Device_info[i])
