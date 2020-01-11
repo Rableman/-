e905e9d993f13ab919d6bad6dc8bb42a2a43e93f
klarss <shadow96neko@gmail.com>
2020-01-11 13:12:58 +0900

    Update map_proc.py

--- search_route/map_proc.py
+++ search_route/map_proc.py
@@ -12,6 +12,16 @@ class map_func:
                 row_new.append(int(i))
             field_name.append(list(row_new))
         f.close()
+        return field_name
+
+    def write_map(file_name,field_name):
+        write_field = list()
+        for row in field_name:
+            row_new = str(row).replace(',','').replace('[','').replace(']','').replace(' ','')
+            write_field.append(row_new)
+        f = open(file_name,'w')
+        f.write('\n'.join(write_field))
+        f.close()
 
     def del_my_goal(object_name):
         x,y = object_name.Goal
@@ -25,16 +35,14 @@ class map_func:
     #たどってきた経路の長さを計算
     def cal_distance(path):
         return len(path)
-
-
-    '''
-    def write_map(self,file_name):
-        device = random_setup.random_set.rand_map(1,5) #今度ランダムじゃないものに置き換える？
-        self.field[device[0]][device[1]] = 1
-
-    def  update_map(self,file_name,object_name,var_name):
-        self.field = list()
-        object_name.load_map(file_name,var_name)
-        for i in range(8):
-            object_name.write_map(file_name)
-    '''
+    
+    def write_step(object_name):
+        for i in range(0,len(object_name.Passed_list)):
+            field = list()
+            x,y = object_name.Passed_list[0]
+            step_name = 'step_' + str(i) + '.txt'
+            field = map_func.load_map(step_name,field)
+            field[x][y] = 1
+            object_name.Passed_list.pop(0)
+            file_name = 'step_' + str(i) + '.txt'
+            map_func.write_map(file_name,field)
