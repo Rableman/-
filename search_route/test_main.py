d8a868b8ff552d1b4d58ecc674cdeaf09626bbd6
klarss <shadow96neko@gmail.com>
2020-01-12 20:57:23 +0900

    Create test_main.py

---
+++ search_route/test_main.py
@@ -0,0 +1,20 @@
+#coding utf-8
+
+import route_proc
+import map_proc
+import goal_proc
+import common_proc
+import route_main
+
+#ローカル変数
+device_num = 1
+
+#この部分がサーバーからゴールのリストが送信される所に変わる
+goal_map = list()
+map_proc.map_func.load_map(common_proc.common_data.select_goal(),goal_map)
+
+#初期化の処理。引数にデバイスの総数、H*Wのフィールド、それぞれのデバイスのスタート地点を渡すと、共通データとデバイス情報のリストを返す
+Common_data,Device_info = route_main.route_main_func.init(3,7,7,(3,4),(3,3),(1,1))
+
+#引数にデバイスの総数とフィールドが入ったCommon_data,デバイスの情報リストのDevice_info,どのデバイスなのか示すdevice_numを渡すと経路が出力
+route_main.route_main_func.main(Common_data,Device_info,device_num,goal_map)
