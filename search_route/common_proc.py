ec46311cb4bd7926a29edee4baaf17a611777b13
klarss <shadow96neko@gmail.com>
2020-01-12 10:52:43 +0900

    Update common_proc.py

--- search_route/common_proc.py
+++ search_route/common_proc.py
@@ -8,27 +8,33 @@ class common_data:
         self.H = h #H*Wのフィールド
         self.W = w
     
+    #デバイスの総数を返す
     def ret_dev_sum(self):
         return self.Device_sum
     
+    #ゴールのフィールドを返す
     def ret_goal_field(self):
         return self.Goal_field
         
+    #次のスタート位置を書き換える
     def set_start(self,object_name):
         object_name.Start = object_name.Next_Start
     
+    #step_n.txtを初期化する
     def init_step(self):
         field = list()
         field = map_proc.map_func.load_map('field.txt',field)
+        #step0~14.txtをfield.txtと同様にすることで初期化
         for i in range(0,15):
             step_name = 'step_' + str(i) + '.txt'
             map_proc.map_func.write_map(step_name,field)
     
-    def init_flag(self,object_name):
-        object_name.flag = 0
-    
+    #ゴールの位置を変更する
     def init_goal(self):
+        #ユーザの入力でゴールのフィールドを選択する
         goal_num = input("please input number : ")
         goal_name = 'goal_' + goal_num + '.txt'
+        #ゴールのフィールドを初期化
         self.Goal_field = list()
+        #ゴールのフィールドを読み込む
         map_proc.map_func.load_map(goal_name,self.Goal_field)
