6e9cef82177d5a247f9d605b507d4d83e50bf1c3
klarss <shadow96neko@gmail.com>
2020-01-13 17:55:34 +0900

    Update route_main.py

--- search_route/route_main.py
+++ search_route/route_main.py
@@ -6,42 +6,42 @@ import goal_proc
 import common_proc
 
 class route_main_func:
-    #初期化の処理
-    #引数にデバイスの総数、H*Wのフィールド、それぞれのデバイスのスタート地点を渡すと、
-    #共通データとデバイス情報のリストを返す
-    def init(device_sum,H,W,Device1_start,Device2_start,Device3_start):
-        #デバイスの総数、フィールドの座標数を設定
-        Common_data = common_proc.common_data(device_sum,H,W)
-        #それぞれのデバイスを初期化
-        Device1 = route_proc.route_data(Common_data.H,Common_data.W,Device1_start)
-        Device2 = route_proc.route_data(Common_data.H,Common_data.W,Device2_start)
-        Device3 = route_proc.route_data(Common_data.H,Common_data.W,Device3_start)
-        #Device_infoというリストにすべてのデバイスのオブジェクトを入れる
-        Device_info = [Device1,Device2,Device3]
-        return Common_data,Device_info
-    
-    #引数にデバイスの総数とフィールドが入ったCommon_data,デバイスの情報リストのDevice_info,
-    #どのデバイスなのか示すdevice_numを渡すと経路を出力
-    def main(Common_data,Device_info,device_num,goal_map):
-        #ゴールのマップを指定
-        Common_data.init_goal(goal_map)
+    #デバイスの総数、フィールドの座標数を設定
+    def __init__(self):
+        self.Device_sum = 3 #デバイスの総数
+        self.H = 7 #H*Wのフィールド
+        self.W = 7
+        #それぞれのデバイスのインスタンスを作る
+        self.Device1 = route_proc.route_data(self.H,self.W,(1,1))
+        self.Device2 = route_proc.route_data(self.H,self.W,(1,2))
+        self.Device3 = route_proc.route_data(self.H,self.W,(1,3))
+        #Device_infoというリストにすべてのデバイスのインスタンスを入れる
+        self.Device_info = [self.Device1,self.Device2,self.Device3]
+
+    #デバイスが何番目なのかというローカル変数と3つのデバイスのスタート地点、ゴールのマップを引数
+    # → 経路を返す
+    def main(self,device_num,Device1_start,Device2_start,Device3_start,goal_map):
+        #それぞれのデバイスのスタート地点を更新
+        common_proc.common_func.set_start(Device1_start,Device2_start,Device3_start,self.Device_info)
         #それぞれのデバイスのゴールを設定する
-        goal_proc.goal_func.search_goal(Common_data.ret_goal_field(),Device_info,Common_data.ret_dev_sum())
+        goal_proc.goal_func.search_goal(goal_map,self.Device_info,self.Device_sum)
         #ループの外でstep_n.txtを初期化する
-        Common_data.init_step()
-        for i in range (Common_data.ret_dev_sum()):
-            #ゴールのフィールドを設定
-            Device_info[i].field = Common_data.Goal_field
-            #デバイスのゴールを決定する
-            map_proc.map_func.del_my_goal(Device_info[i])
+        common_proc.common_func.init_step()
+        for i in range (self.Device_sum):
+            #ゴールのフィールドをフィールドとして設定
+            self.Device_info[i].field = goal_map
+            #自分のデバイスの位置を削除
+            map_proc.map_func.del_my_goal(self.Device_info[i])
             #デバイスのルートを策定する
-            Device_info[i].search_route(Device_info[i],Device_info[i].field)
+            self.Device_info[i].search_route(self.Device_info[i],self.Device_info[i].field)
             #通る道を1で埋める
-            Device_info[i].update_route()
+            self.Device_info[i].update_route()
             #ステップに書き込み
-            map_proc.map_func.write_step(Device_info[i])
+            map_proc.map_func.write_step(self.Device_info[i])
             if (i == device_num-1):
-                #ルートを表示
-                Device_info[i].show_route()
+                #ルートを出力
+                print_route = self.Device_info[i].show_route()
             #初期化
-            Device_info[i].init_data(Common_data.H,Common_data.W)
+            self.Device_info[i].init_data(self.H,self.W)
+        #探索したルートを返り値にする
+        return print_route
