4d095b18710db6f5a4f4576f0c5eee03ca217407
klarss <shadow96neko@gmail.com>
2020-01-13 17:55:27 +0900

    Update common_proc.py

--- search_route/common_proc.py
+++ search_route/common_proc.py
@@ -1,27 +1,10 @@
 #coding utf-8
 import map_proc
 
-class common_data:
-    def __init__(self,device_num,h,w):
-        self.Device_sum = device_num #デバイスの総数
-        self.Goal_field = list() #ゴールのフィールド
-        self.H = h #H*Wのフィールド
-        self.W = w
-    
-    #デバイスの総数を返す
-    def ret_dev_sum(self):
-        return self.Device_sum
-    
-    #ゴールのフィールドを返す
-    def ret_goal_field(self):
-        return self.Goal_field
-        
-    #次のスタート位置を書き換える
-    def set_start(self,object_name):
-        object_name.Start = object_name.Next_Start
+class common_func:
     
     #step_n.txtを初期化する
-    def init_step(self):
+    def init_step():
         field = list()
         field = map_proc.map_func.load_map('field.txt',field)
         #step0~14.txtをfield.txtと同様にすることで初期化
@@ -36,9 +19,8 @@ class common_data:
         goal_name = 'goal_' + goal_num + '.txt'
         return goal_name
     
-    #ゴールの位置を変更する
-    def init_goal(self,goal_map):
-        #ゴールのフィールドを初期化
-        self.Goal_field = list()
-        #ゴールのフィールドを変更
-        self.Goal_field = goal_map
+    #スタート地点の設定
+    def set_start(Device1_start,Device2_start,Device3_start,Device_info):
+        Device_info[0].Start = Device1_start
+        Device_info[1].Start = Device2_start
+        Device_info[2].Start = Device3_start
