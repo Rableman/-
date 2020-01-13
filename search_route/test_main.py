dd9d099e2b5f64f35fbad31f898f91641288d468
klarss <shadow96neko@gmail.com>
2020-01-13 17:55:39 +0900

    Update test_main.py

--- search_route/test_main.py
+++ search_route/test_main.py
@@ -2,13 +2,35 @@
 
 import route_proc
 import map_proc
-import goal_proc
 import common_proc
 import route_main
 
 #ローカル変数
 device_num = 1
+#route_mainをオブジェクト化
+Route_main = route_main.route_main_func()
 
+#この部分がサーバーからゴールのリストが送信される所に変わる
+goal_map = list()
+map_proc.map_func.load_map(common_proc.common_func.select_goal(),goal_map)
+
+#デバイスが何番目なのかというローカル変数と3つのデバイスのスタート地点、ゴールのマップを引数 → 経路を返す
+a = Route_main.main(device_num,(1,1),(1,2),(1,3),goal_map)
+print(a)
+
+#この部分がサーバーからゴールのリストが送信される所に変わる
+goal_map = list()
+map_proc.map_func.load_map(common_proc.common_func.select_goal(),goal_map)
+
+#デバイスが何番目なのかというローカル変数と3つのデバイスのスタート地点、ゴールのマップを引数 → 経路を返す
+a = Route_main.main(device_num,(2,1),(1,2),(1,3),goal_map)
+print(a)
+
+
+
+
+
+'''
 #この部分がサーバーからゴールのリストが送信される所に変わる
 goal_map = list()
 map_proc.map_func.load_map(common_proc.common_data.select_goal(),goal_map)
@@ -18,3 +40,4 @@ Common_data,Device_info = route_main.route_main_func.init(3,7,7,(3,4),(3,3),(1,1
 
 #引数にデバイスの総数とフィールドが入ったCommon_data,デバイスの情報リストのDevice_info,どのデバイスなのか示すdevice_numを渡すと経路が出力
 route_main.route_main_func.main(Common_data,Device_info,device_num,goal_map)
+'''
