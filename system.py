a3a1a275f59987057120597964b6eda9902fc204
wkpmr <hamu.110203@gmail.com>
2020-01-13 20:34:35 +0900

    route to move

--- system.py
+++ system.py
@@ -1,6 +1,7 @@
 from sond_localization import calcdist
 from communication import commu
 from search_route import route_main
+from stepper-raspi import popen
 
 if __name__=="__main__":"
     dev_num = int(input("input device numver: "))
@@ -10,20 +11,30 @@ if __name__=="__main__":"
     x, y = 0, 0
     serverip = "172.31.150.3"
     
-    def motion(x, y, route):
-        while True:
-            next_x, next_y = next(x, y, route)
-            dif_x = next_x - x
-            dif_y = next_y - y
-            if dif_x < 0 and dif_y == 0:
-                move("R")
-            elif dif_x > 0 and dif_Y == 0:
-                move("L")
-            elif dif_y > 0 and dif_x == 0:
-                move("S")
-            elif dif_x == 0 and dif_Y == 0:
-                print("GOAL")
-                break
+    def motion(start, goal, route):
+        routeobj = [[y for y in x] for x in route.split("\n")]
+        ada = search_adapter.adapter(start,goal,route)
+        nowp = start
+        prevp = [start[0], start[1]]
+        while nexp != goal:
+            nexp = ada.next()
+            dif_x = nexp[0] - nowp[0]
+            dif_y = nexp[1] - nowp[1]
+            if abs(nexp[0] - prep[0]) == 2 or abs(nexp[1] - prep[1]) == 2:
+                popen.movefor("s")
+            elif (nowp[0] - prep[0] > 0 and nexp[1] - nowp[1] < 0) or
+                (nowp[0] - prep[0] < 0 and nexp[1] - nowp[1] > 0) or
+                (nowp[1] - prep[1] > 0 and nexp[0] - nowp[0] > 0) or
+                (nowp[1] - prep[1] < 0 and nexp[0] - nowp[0] < 0):
+                popen.movefor("l")
+            elif (nowp[0] - prep[0] > 0 and nexp[1] - nowp[1] > 0) or
+                (nowp[0] - prep[0] < 0 and nexp[1] - nowp[1] < 0) or
+                (nowp[1] - prep[1] > 0 and nexp[0] - nowp[0] < 0) or
+                (nowp[1] - prep[1] < 0 and nexp[0] - nowp[0] > 0):
+                popen.movefor("r")
+            prep = nowp
+            nowp = nexp
+        print("GOAL")
 
 
     while(1):
@@ -35,7 +46,7 @@ if __name__=="__main__":"
         else:
             x, y = loc.getcoord()
             data = commu.communication("tcp", serverip, 50007).client(str(x) + "," + str(y))
-            route =search.seachr(dev_num, (x1, y1), (x2, y2), (x3, y3), map)
+            Gpoint, route =search.seachr(dev_num, (x1, y1), (x2, y2), (x3, y3), map)
 
-            motion(x, y, route)
+            motion([x, y], Gpoint, route)
 
