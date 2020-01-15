ecfcb793163b1ca171099e3f7359c5cc37ec7a03
jk <hamu.110203@gmail.com>
2020-01-15 11:23:54 +0000

    system fix

--- system.py
+++ system.py
@@ -9,8 +9,8 @@ def motion(start, goal, route):
     ada = search_adapter.adapter(start,goal,routeobj)
     nowp = start
     prep = [start[0], start[1]]
+    nexp = nowp
     while nexp != goal:
-        nexp = ada.next()
         dif_x = nexp[0] - nowp[0]
         dif_y = nexp[1] - nowp[1]
         if abs(nexp[0] - prep[0]) == 2 or abs(nexp[1] - prep[1]) == 2:
@@ -20,12 +20,13 @@ def motion(start, goal, route):
         elif (nowp[0] - prep[0] > 0 and nexp[1] - nowp[1] > 0) or (nowp[0] - prep[0] < 0 and nexp[1] - nowp[1] < 0) or (nowp[1] - prep[1] > 0 and nexp[0] - nowp[0] < 0) or (nowp[1] - prep[1] < 0 and nexp[0] - nowp[0] > 0):
             popen.movefor("r")
         prep = nowp
+        nexp = ada.next()
         nowp = nexp
     print("GOAL")
 
 
 if __name__=="__main__":
-    dev_num = int(input("input device numver: "))
+    dev_num = int(input("input device number: "))
     loc = calcdist.Calcdist()
     search =  route_main.route_main_func()
     map = []
