75f17adcb5072a516f5ced8053f8f9d2b4dd74b3
Rableman <haruki_yokota@hotmail.co.jp>
2020-01-15 17:55:44 +0900

    system fix

--- sever.py
+++ sever.py
@@ -3,4 +3,4 @@ from communication import commu
 filenum = input("select map: ")
 f = open(filenum, "r")
 data = f.read(512)
-print(commu.communication("tcp", "127.0.0.1", 50007).server(data)
+print(commu.communication("tcp", "172.31.150.11", 50007).server(data)
--- system.py
+++ system.py
@@ -3,6 +3,30 @@ from communication import commu
 from search_route import route_main
 from stepper_raspi import popen
 
+def motion(start, goal, route):
+    routeobj = [[y for y in x] for x in route.split("\n")]
+    ada = search_adapter.adapter(start,goal,route)
+    nowp = start
+    prevp = [start[0], start[1]]
+    while nexp != goal:
+        nexp = ada.next()
+        dif_x = nexp[0] - nowp[0]
+        dif_y = nexp[1] - nowp[1]
+        if abs(nexp[0] - prep[0]) == 2 or abs(nexp[1] - prep[1]) == 2:
+            popen.movefor("s")
+        elif (nowp[0] - prep[0] > 0 and nexp[1] - nowp[1] < 0) or (nowp[0] - prep[0] < 0 and nexp[1] - nowp[1] > 0) or
+            (nowp[1] - prep[1] > 0 and nexp[0] - nowp[0] > 0) or
+            (nowp[1] - prep[1] < 0 and nexp[0] - nowp[0] < 0):
+            popen.movefor("l")
+        elif (nowp[0] - prep[0] > 0 and nexp[1] - nowp[1] > 0) or
+            (nowp[0] - prep[0] < 0 and nexp[1] - nowp[1] < 0) or
+            (nowp[1] - prep[1] > 0 and nexp[0] - nowp[0] < 0) or
+            (nowp[1] - prep[1] < 0 and nexp[0] - nowp[0] > 0):
+            popen.movefor("r")
+        prep = nowp
+        nowp = nexp
+    print("GOAL")
+
 if __name__=="__main__":
     dev_num = int(input("input device numver: "))
     loc = calcdist.Calcdist()
@@ -10,31 +34,6 @@ if __name__=="__main__":
     map = []
     x, y = 0, 0
     serverip = "172.31.150.3"
-    
-    def motion(start, goal, route):
-        routeobj = [[y for y in x] for x in route.split("\n")]
-        ada = search_adapter.adapter(start,goal,route)
-        nowp = start
-        prevp = [start[0], start[1]]
-        while nexp != goal:
-            nexp = ada.next()
-            dif_x = nexp[0] - nowp[0]
-            dif_y = nexp[1] - nowp[1]
-            if abs(nexp[0] - prep[0]) == 2 or abs(nexp[1] - prep[1]) == 2:
-                popen.movefor("s")
-            elif (nowp[0] - prep[0] > 0 and nexp[1] - nowp[1] < 0) or (nowp[0] - prep[0] < 0 and nexp[1] - nowp[1] > 0) or
-                (nowp[1] - prep[1] > 0 and nexp[0] - nowp[0] > 0) or
-                (nowp[1] - prep[1] < 0 and nexp[0] - nowp[0] < 0):
-                popen.movefor("l")
-            elif (nowp[0] - prep[0] > 0 and nexp[1] - nowp[1] > 0) or
-                (nowp[0] - prep[0] < 0 and nexp[1] - nowp[1] < 0) or
-                (nowp[1] - prep[1] > 0 and nexp[0] - nowp[0] < 0) or
-                (nowp[1] - prep[1] < 0 and nexp[0] - nowp[0] > 0):
-                popen.movefor("r")
-            prep = nowp
-            nowp = nexp
-        print("GOAL")
-
 
     while(1):
         data = commu.communication("tcp", serverip, 50007).client("1")
