fd09425c6000529ff9684321577c9a1dac3ac793
Rableman <haruki_yokota@hotmail.co.jp>
2020-01-15 15:58:16 +0900

    調整

--- sound_local/calcdist.py
+++ sound_local/calcdist.py
@@ -35,10 +35,10 @@ class Calcdist:
     
     def get_dist(self, debug =True):
         sec = 2 #録音時間
-        dist=[]
+        dist=[0,0]
         source=["A","B"]
-        dist.append = self.calc_dist(self.rec.record(self.freqA,sec), self.freqA)
-        dist.append = self.calc_dist(self.rec.record(self.freqB,sec), self.freqB)
+        dist[0]= self.calc_dist(self.rec.record(self.freqA,sec), self.freqA)
+        dist[1] = self.calc_dist(self.rec.record(self.freqB,sec), self.freqB)
         if debug == True: 
             for i in range(2):
                 print("Dist_%s: %d[cm]" % source[i], dist[i])
@@ -51,28 +51,12 @@ class Calcdist:
             ret = self.funcA[0] * math.log(amp) + self.funcA[1]
         elif freq == self.freqB:
             ret = self.funcB[0] * math.log(amp) + self.funcB[1]
-
         return ret
-    
-    #座標初期化
-    def init_grid(self):
-        grid_num = 5
-        grid = [[0 for i in range(grid_num)] for i in range(grid_num)]
-        return grid
-
-    #座標表示
-    def show_grid(self):
-        self.set_grid(0,0)
-        for x in range(5):
-            print(self.grid[x])
 
     #座標提供
     def get_coord(self):
         dist = self.get_dist()        
         x, y = self.calc_coord(dist[0], self.cos_rule(dist[0], dist[1], self.sourceB[0] - self.sourceA[0]))
-        #debug
-        #self.grid[x][y] = 1
-        #self.show_grid()
         return x, y
 
     #座標算出
